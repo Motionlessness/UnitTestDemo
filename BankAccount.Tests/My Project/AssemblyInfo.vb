094873c9e400749dad1254d65823c00a9fa58a19
Wil Crouch <william.crouch@hotmail.com>
2020-07-30 08:40:04 -0700

    Add project files.

---
+++ BankAccount.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("BankAccount.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BankAccount.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("5e2c3167-3cd9-4d3f-96fa-fd7103c6e900")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ BankAccount/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Account
+    { 
+        private double _balance;
+
+        /// <summary>
+        /// deposits the amount in the bank account and returns the new balance
+        /// </summary>
+        /// <param name="amt">the amount to deposit</param>
+        /// <returns></returns>
+        public double Deposit(double amt)
+        {
+            _balance += amt;
+            return _balance;
+        }
+
+         public double Withdraw(double amt)
+         {
+            _balance -= amt;
+            return _balance;
+         }
+            
+    }
+}
